0394d04ea5e9a8f9283338248165c886cff2226f
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-05-27 13:11:49 -0400

    test

---
+++ RoomBookingApp/RoomBookingResult.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingResult
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOnly { get; set; }
+    }
+}
