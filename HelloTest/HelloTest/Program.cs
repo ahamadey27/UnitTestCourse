e15c553434044b4908a12e9c8d00e56692870b31
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-05-31 20:08:31 -0400

    more testing

---
+++ HelloTest/HelloTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HelloTest/HelloXunit/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloXunit;
+
+using System;
+using Xunit;
+using System.IO;
+using HelloTest;
+
+
+public class UnitTest1
+{
+    private const string Expected = "Hello World!";
+    [Fact]
+    public void Test1()
+    {
+        using (var sw = new StringWriter())
+        {
+            Console.SetOut(sw);
+            HelloTest.Program.Main();
+
+            var result = sw.ToString().Trim();
+            Assert.Equal(Expected, result);
+        }
+    }
+}
